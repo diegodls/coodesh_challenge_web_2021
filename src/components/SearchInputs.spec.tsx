6b0187df6f02cd4422f84fe1f67d4ac6d3813680
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-05 14:55:25 -0300

    test - testing search input

--- src/components/SearchInputs.spec.tsx
+++ src/components/SearchInputs.spec.tsx
@@ -1,7 +1,17 @@
-import react from "react";
+import { fireEvent, render, screen } from "../test/testing-library-utils";
+import SearchInputs from "./SearchInputs";
 
 describe("Testing SearchInputs component", () => {
-  it("should show name search input", () => {
-    expect(1 + 1).toBe(2);
+  it("should show name search by name input", () => {
+    render(<SearchInputs />);
+    const searchInput: HTMLInputElement = screen.getByPlaceholderText(
+      /Pesquisar paciente.../i
+    );
+
+    expect(searchInput).toBeInTheDocument();
+
+    fireEvent.change(searchInput, { target: { value: "test" } });
+
+    expect(searchInput.value).toBe("test");
   });
 });
