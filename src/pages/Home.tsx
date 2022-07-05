324ac230a510fa3f8457acd22857b3eda0d0a01e
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-05 14:50:11 -0300

    refact - removing context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,7 @@ import PatientModal from "../components/PatientModal";
 import PatientTableList from "../components/PatientTableList";
 import SearchInputs from "../components/SearchInputs";
 
-import { usePatientContext } from "../contexts/usePatientsContext";
-
 const Home = () => {
-  const {
-    genderFilter,
-    natFilter,
-    nameFilter,
-    currentFilters,
-    lastFilters,
-    loadingPatients,
-    loadMorePatients,
-    handleChangeGenderFilter,
-    setNatFilter,
-    handleChangeNameFilter,
-  } = usePatientContext();
-
   return (
     <div className='w-11/12 h-full flex flex-col items-center gap-2 px-3'>
       <p>
@@ -25,18 +10,7 @@ const Home = () => {
         pacientes. Aqui você poderá localizar, filtrar e ver os detalhes de
         nossos pacientes.
       </p>
-      <SearchInputs
-        currentFilters={currentFilters}
-        genderFilter={genderFilter}
-        lastFilters={lastFilters}
-        loadingPatients={loadingPatients}
-        natFilter={natFilter}
-        nameFilter={nameFilter}
-        loadMorePatients={loadMorePatients}
-        setNatFilter={setNatFilter}
-        handleChangeNameFilter={handleChangeNameFilter}
-        handleChangeGenderFilter={handleChangeGenderFilter}
-      />
+      <SearchInputs />
       <PatientTableList />
       <PatientModal />
     </div>
