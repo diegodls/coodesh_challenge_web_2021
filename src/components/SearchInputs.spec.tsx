c60953bd048b3e7ae293e8ab609bbc6a9af470ca
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-06 11:36:27 -0300

    test - add test for genders checkbox

--- src/components/SearchInputs.spec.tsx
+++ src/components/SearchInputs.spec.tsx
@@ -1,8 +1,10 @@
-import { fireEvent, render, screen } from "../test/testing-library-utils";
+import { render, screen, waitFor } from "../test/testing-library-utils";
+import userEvent from "@testing-library/user-event";
+
 import SearchInputs from "./SearchInputs";
 
 describe("Testing SearchInputs component", () => {
-  it("should show name search by name input", () => {
+  it("should render name search by name input", async () => {
     render(<SearchInputs />);
     const searchInput: HTMLInputElement = screen.getByPlaceholderText(
       /Pesquisar paciente.../i
@@ -10,8 +12,51 @@ describe("Testing SearchInputs component", () => {
 
     expect(searchInput).toBeInTheDocument();
 
-    fireEvent.change(searchInput, { target: { value: "test" } });
+    userEvent.clear(searchInput);
+    userEvent.type(searchInput, "test");
 
-    expect(searchInput.value).toBe("test");
+    waitFor(() => {
+      expect(searchInput.value).toBe("test");
+    });
+  });
+
+  it("should filters options starts hidden", () => {
+    render(<SearchInputs />);
+
+    const femaleCheckbox: HTMLInputElement | null = screen.queryByRole(
+      "checkbox",
+      {
+        name: "Feminino",
+      }
+    );
+
+    expect(femaleCheckbox).not.toBeInTheDocument();
+
+    const maleCheckbox: HTMLInputElement | null = screen.queryByRole(
+      "checkbox",
+      {
+        name: "Feminino",
+      }
+    );
+
+    expect(maleCheckbox).not.toBeInTheDocument();
+  });
+
+  it("should shown filters options when click on shown button", async () => {
+    render(<SearchInputs />);
+
+    const showFiltersButton = screen.getByRole("button", {
+      name: /show-filters/i,
+    });
+
+    expect(showFiltersButton).toBeInTheDocument();
+
+    userEvent.click(showFiltersButton);
+
+    const femaleCheckbox = await screen.findByRole("checkbox", {
+      name: /feminino/i,
+    });
+
+    expect(femaleCheckbox).toBeInTheDocument();
   });
 });
