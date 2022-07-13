58d50f8cb29438bb051767ced4f13a2cc4823b09
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-13 16:58:42 -0300

    refac - del unused code + add separate components

--- src/components/PatientTableArea.tsx
+++ src/components/PatientTableArea.tsx
@@ -1,71 +1,50 @@
-import { ArrowClockwise, CircleNotch } from "phosphor-react";
-import { PatientFullData } from "../interfaces/IPatient";
+import React, { FC, ReactElement } from "react";
+
+import { ArrowClockwise } from "phosphor-react";
 
 import { usePatientContext } from "../contexts/usePatientsContext";
+import { ErrorOnFetchPatients } from "./ErrorOnFetchPatients";
 
-import { usePatientModal } from "../contexts/useModalPatients";
-
-import LoadingPatients from "./LoadingPatients";
-import { SortingButtonTable } from "./SortingButtonTable";
-import PatientTableList from "./PatientTableList";
-
-interface OrderByTypes {
-  name: string;
-  gender: string;
-  dob: string;
-}
+import { LoadingFetchPatients } from "./LoadingFetchPatients";
+import { PatientTable } from "./PatientTable";
+import { ErrorPatientNotFound } from "./ErrorPatientNotFound";
 
 export const PatientTableArea = () => {
-  const { setPatient } = usePatientModal();
-
   const {
     filteredPatientsList,
-    errorLoadingPatients,
     loadingPatients,
-    order,
-    orderBy,
+    errorLoadingPatients,
     loadMorePatients,
-    defineTypeOfSorting,
   } = usePatientContext();
 
-  function setTypeOfSorting(type: keyof OrderByTypes) {
-    defineTypeOfSorting(type);
+  let component: ReactElement = <PatientTable />;
+
+  if (
+    loadingPatients &&
+    filteredPatientsList &&
+    filteredPatientsList?.length <= 0
+  ) {
+    component = <LoadingFetchPatients />;
   }
 
-  function viewPatientDetails(patient: PatientFullData): void {
-    setPatient(patient);
+  if (errorLoadingPatients) {
+    component = <ErrorOnFetchPatients />;
   }
 
-  function formatDate(date: string): string {
-    let tempDate: Date = new Date(date);
-
-    let stringDay: string =
-      tempDate.getDate() < 10
-        ? `0${tempDate.getDate()}`
-        : `${tempDate.getDate()}`;
-
-    let stringMonth: string =
-      tempDate.getMonth() + 1 < 10
-        ? `0${tempDate.getMonth() + 1}`
-        : `${tempDate.getMonth() + 1}`;
-
-    let stringYear: string = String(tempDate.getFullYear());
-
-    return `${stringDay}/${stringMonth}/${stringYear}`;
+  if (
+    !loadingPatients &&
+    !errorLoadingPatients &&
+    filteredPatientsList &&
+    filteredPatientsList.length === 0
+  ) {
+    component = <ErrorPatientNotFound />;
   }
 
   return (
     <div className='w-full flex flex-col items-center justify-center mt-1 '>
-      <div className='w-full h-[450px] flex flex-col items-center justify-center'>
-        {loadingPatients ? (
-          <LoadingPatients />
-        ) : (
-          <div className='w-full h-[440px] rounded border border-solid border-pharma-border_enable overflow-auto scrollbar-thumb-zinc-500 hover:scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'>
-            <PatientTableList />
-          </div>
-        )}
+      <div className='w-full h-[440px] flex flex-col items-center justify-center'>
+        {component}
       </div>
-
       <div className='w-full flex flex-col items-center justify-center '>
         <button
           className='h-full flex flex-row items-center justify-center border-0 rounded gap-1 text-center disabled:opacity-50'
