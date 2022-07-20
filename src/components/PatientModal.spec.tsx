6293890e2c4b76d244d5535095b7871247e6b127
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-20 15:02:12 -0300

    test - improving test coverage

--- src/components/PatientModal.spec.tsx
+++ src/components/PatientModal.spec.tsx
@@ -9,6 +9,9 @@ import * as ModalContext from "../contexts/useModalPatients";
 import { PatientFullData } from "../interfaces/IPatient";
 import { patientListOnePageMock } from "../test/patientListMock";
 
+const femalePatient: PatientFullData = patientListOnePageMock.results[0];
+const malePatient: PatientFullData = patientListOnePageMock.results[1];
+
 describe("Testing PatientModal.tsx", () => {
   it("shouldn't display modal when no patient is set", async () => {
     const currentModalPatient: PatientFullData | null = null;
@@ -37,9 +40,8 @@ describe("Testing PatientModal.tsx", () => {
     expect(patientName).not.toBeInTheDocument();
   });
 
-  it("should display modal when patient is set", async () => {
-    const currentModalPatient: PatientFullData =
-      patientListOnePageMock.results[0];
+  it("should display modal when female patient is set", async () => {
+    const currentModalPatient: PatientFullData = femalePatient;
 
     const openPatientModal = true;
     const setPatient = jest.fn();
@@ -70,6 +72,46 @@ describe("Testing PatientModal.tsx", () => {
       /cecilia.wojcik@example.com/i
     );
     expect(patientEmail).toBeInTheDocument();
+
+    const patientGender = await RTLScreen.findByText(/feminino/i);
+    expect(patientGender).toBeInTheDocument();
+  });
+
+  it("should display modal when male patient is set", async () => {
+    const currentModalPatient: PatientFullData = malePatient;
+
+    const openPatientModal = true;
+    const setPatient = jest.fn();
+    const handleClose = jest.fn();
+    const setOpenPatientModal = jest.fn();
+
+    const contextValues = {
+      currentModalPatient,
+      openPatientModal,
+      setPatient,
+      handleClose,
+      setOpenPatientModal,
+    };
+
+    jest
+      .spyOn(ModalContext, "usePatientModal")
+      .mockImplementation(() => contextValues);
+
+    RTLRender(<PatientModal />);
+
+    const patientName = await RTLScreen.findByText(/Daniel Zhang/i);
+    expect(patientName).toBeInTheDocument();
+
+    const emailField = await RTLScreen.findByText(/Email:/i);
+    expect(emailField).toBeInTheDocument();
+
+    const patientEmail = await RTLScreen.findByText(
+      /daniel.zhang@example.com/i
+    );
+    expect(patientEmail).toBeInTheDocument();
+
+    const patientGender = await RTLScreen.findByText(/masculino/i);
+    expect(patientGender).toBeInTheDocument();
   });
 
   it("should display error message when patient is not set", async () => {
