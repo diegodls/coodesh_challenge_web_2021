fc6b43f67dc467a8d2460cbc1ebdb28c17333aee
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-20 15:00:17 -0300

    refac - add test to input's value

--- src/components/NameSearchOption.spec.tsx
+++ src/components/NameSearchOption.spec.tsx
@@ -1,10 +1,24 @@
-import { render, screen } from "@testing-library/react";
-import NameSearchOption from "./nameSearchOption";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "../test/testing-library-utils";
+
+import NameSearchOption from "./NameSearchOption";
 
 describe("Testing NameSearchOption.tsx", () => {
   it("should render a text input", () => {
     render(<NameSearchOption />);
     const nameInput = screen.getByPlaceholderText(/Pesquisar paciente/i);
     expect(nameInput).toBeInTheDocument();
+  });
+
+  it("should be able to type in text input", async () => {
+    render(<NameSearchOption />);
+    const nameInput = screen.getByPlaceholderText(/Pesquisar paciente/i);
+
+    expect(nameInput).toBeInTheDocument();
+
+    await userEvent.clear(nameInput);
+    await userEvent.type(nameInput, "test");
+
+    expect(nameInput).toHaveValue("test");
   });
 });
