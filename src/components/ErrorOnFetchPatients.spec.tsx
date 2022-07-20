92787e4ea5ebf710ee68c1bb1b4ee2ffcc7b67ba
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-20 14:59:13 -0300

    refac - improving test coverage

--- src/components/ErrorOnFetchPatients.spec.tsx
+++ src/components/ErrorOnFetchPatients.spec.tsx
@@ -1,14 +1,47 @@
-import { render, screen } from "../test/testing-library-utils";
+import {
+  render as RTLRender,
+  screen as RTLScreen,
+} from "@testing-library/react";
 import { ErrorOnFetchPatients } from "./ErrorOnFetchPatients";
+import * as PatientContext from "../contexts/usePatientsContext";
+import "intersection-observer";
 
 describe("Testing LoadingPatients.tsx", () => {
   it("should display error message", async () => {
-    render(<ErrorOnFetchPatients />);
+    const contextValues: PatientContext.PatientContextData = {
+      errorLoadingPatients: "ErrorTest",
+      patientsList: null,
+      filteredPatientsList: null,
+      loadingPatients: false,
+      nameFilter: "",
+      genderFilter: null,
+      natFilter: [],
+      currentFilters: "",
+      lastFilters: "",
+      order: "asc",
+      orderBy: "name",
+      loadMorePatients: jest.fn(),
+      defineTypeOfSorting: jest.fn(),
+      handleChangeGenderFilter: jest.fn(),
+      setNatFilter: jest.fn(),
+      handleChangeNameFilter: jest.fn(),
+      handleChangePatientQuantity: jest.fn(),
+    };
 
-    const errorMessage = screen.getByText(
+    jest
+      .spyOn(PatientContext, "usePatientContext")
+      .mockImplementation(() => contextValues);
+
+    RTLRender(<ErrorOnFetchPatients />);
+
+    const errorMessage = RTLScreen.getByText(
       "Erro ao carregar a lista de pacientes."
     );
 
     expect(errorMessage).toBeInTheDocument();
+
+    const errorType = RTLScreen.getByText(/Erro: ErrorTest/i);
+
+    expect(errorType).toBeInTheDocument();
   });
 });
