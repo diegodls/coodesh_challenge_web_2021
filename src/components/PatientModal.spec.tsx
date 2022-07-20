2ae289ea7f609a7927c740be929a7f7b6ceb7030
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-20 11:20:59 -0300

    test - testing errors on modal

--- src/components/PatientModal.spec.tsx
+++ src/components/PatientModal.spec.tsx
@@ -37,7 +37,7 @@ describe("Testing PatientModal.tsx", () => {
     expect(patientName).not.toBeInTheDocument();
   });
 
-  it("should display modal", async () => {
+  it("should display modal when patient is set", async () => {
     const currentModalPatient: PatientFullData =
       patientListOnePageMock.results[0];
 
@@ -61,7 +61,44 @@ describe("Testing PatientModal.tsx", () => {
     RTLRender(<PatientModal />);
 
     const patientName = await RTLScreen.findByText(/Cecilia Wojcik/i);
-
     expect(patientName).toBeInTheDocument();
+
+    const emailField = await RTLScreen.findByText(/Email:/i);
+    expect(emailField).toBeInTheDocument();
+
+    const patientEmail = await RTLScreen.findByText(
+      /cecilia.wojcik@example.com/i
+    );
+    expect(patientEmail).toBeInTheDocument();
+  });
+
+  it("should display error message when patient is not set", async () => {
+    const currentModalPatient: PatientFullData | null = null;
+    const openPatientModal = true;
+    const setPatient = jest.fn();
+    const handleClose = jest.fn();
+    const setOpenPatientModal = jest.fn();
+
+    const contextValues = {
+      currentModalPatient,
+      openPatientModal,
+      setPatient,
+      handleClose,
+      setOpenPatientModal,
+    };
+
+    jest
+      .spyOn(ModalContext, "usePatientModal")
+      .mockImplementation(() => contextValues);
+
+    RTLRender(<PatientModal />);
+
+    const errorMessage = await RTLScreen.findByText(
+      /Erro ao carregar os dados do paciente!/i
+    );
+    expect(errorMessage).toBeInTheDocument();
+
+    const emailField = RTLScreen.queryByText(/Email:/i);
+    expect(emailField).not.toBeInTheDocument();
   });
 });
