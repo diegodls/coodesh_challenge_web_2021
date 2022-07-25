5eb707932a467023d66403d4cddca24bb59961b4
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-25 16:11:50 -0300

    feat - creating project and test

---
+++ src/components/Header.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Testing Header,tsx", () => {
+  it("should render the header title", () => {
+    render(<Header />);
+
+    const title = screen.getByText(/Pharma Inc./i);
+
+    expect(title).toBeInTheDocument();
+  });
+});
