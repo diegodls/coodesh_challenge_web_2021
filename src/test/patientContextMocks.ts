06ab92793416f600065ec049653c0aa3c73a5fe8
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-26 15:02:33 -0300

    refac - changing mock file path

---
+++ src/test/patientContextMocks.ts
@@ -0,0 +1,604 @@
+import { PatientContextData } from "../contexts/usePatientsContext";
+import { ApiResponseComplete } from "../interfaces/IPatient";
+
+export const patientContextMockValues: PatientContextData = {
+  errorLoadingPatients: "ErrorTest",
+  patientsList: null,
+  filteredPatientsList: null,
+  loadingPatients: false,
+  nameFilter: "",
+  genderFilter: null,
+  natFilter: [],
+  currentFilters: "",
+  lastFilters: "",
+  order: "asc",
+  orderBy: null,
+  loadMorePatients: jest.fn(),
+  handleApplyFilters: jest.fn(),
+  defineTypeOfSorting: jest.fn(),
+  handleChangeGenderFilter: jest.fn(),
+  setNatFilter: jest.fn(),
+  handleChangeNameFilter: jest.fn(),
+  handleChangePatientQuantity: jest.fn(),
+};
+
+export const patientListOnePageMock: ApiResponseComplete = {
+  results: [
+    {
+      gender: "female",
+      name: {
+        title: "Ms",
+        first: "Cecilia",
+        last: "Wojcik",
+      },
+      location: {
+        street: {
+          number: 7468,
+          name: "Hagaveien",
+        },
+        city: "Hauge",
+        state: "Hordaland",
+        country: "Norway",
+        postcode: "5213",
+        coordinates: {
+          latitude: "52.8369",
+          longitude: "107.6127",
+        },
+        timezone: {
+          offset: "+10:00",
+          description: "Eastern Australia, Guam, Vladivostok",
+        },
+      },
+      email: "cecilia.wojcik@example.com",
+      login: {
+        uuid: "2fdeb445-9ab5-4d5a-a9fa-55e7c008581b",
+        username: "purplegoose386",
+        password: "weird",
+        salt: "QRwZwP5K",
+        md5: "bdb2b09c3ce635ea6c2618fbcc68cb01",
+        sha1: "eb73de15d5889c77dbd8a0224b4071a3a2a0cff9",
+        sha256:
+          "b4f6032521e19a935be77f775a3be669430214b561df3af1ea29b7e4f8471d93",
+      },
+      dob: {
+        date: "1994-12-04T19:18:25.025Z",
+        age: 28,
+      },
+      registered: {
+        date: "2010-03-11T00:45:44.251Z",
+        age: 12,
+      },
+      phone: "20535160",
+      cell: "47306734",
+      id: {
+        name: "FN",
+        value: "04129449042",
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/74.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/74.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/74.jpg",
+      },
+      nat: "NO",
+    },
+    {
+      gender: "male",
+      name: {
+        title: "Mr",
+        first: "Daniel",
+        last: "Zhang",
+      },
+      location: {
+        street: {
+          number: 9329,
+          name: "Taieri Road",
+        },
+        city: "Greymouth",
+        state: "Waikato",
+        country: "New Zealand",
+        postcode: 41813,
+        coordinates: {
+          latitude: "-85.6699",
+          longitude: "-87.8798",
+        },
+        timezone: {
+          offset: "-4:00",
+          description: "Atlantic Time (Canada), Caracas, La Paz",
+        },
+      },
+      email: "daniel.zhang@example.com",
+      login: {
+        uuid: "b509c56b-08d4-44f4-ad3d-535ca86863d3",
+        username: "smallduck575",
+        password: "clutch",
+        salt: "muXPGzHy",
+        md5: "614694a71cf5f19255570fc0b6528bd8",
+        sha1: "2ec63249c1cda3630d737cd5a5445f2e8b917f31",
+        sha256:
+          "dbd87df75bfca10052bff87a521c213808aed87434bf7c994bbac502ec744133",
+      },
+      dob: {
+        date: "1990-09-06T10:22:51.219Z",
+        age: 32,
+      },
+      registered: {
+        date: "2018-08-24T08:20:38.878Z",
+        age: 4,
+      },
+      phone: "(537)-097-9907",
+      cell: "(742)-737-3139",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/4.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/4.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/4.jpg",
+      },
+      nat: "NZ",
+    },
+    {
+      gender: "male",
+      name: {
+        title: "Mr",
+        first: "آدرین",
+        last: "کامروا",
+      },
+      location: {
+        street: {
+          number: 3963,
+          name: "راستوان",
+        },
+        city: "بوشهر",
+        state: "خراسان شمالی",
+        country: "Iran",
+        postcode: 27872,
+        coordinates: {
+          latitude: "5.1223",
+          longitude: "126.3168",
+        },
+        timezone: {
+          offset: "-9:00",
+          description: "Alaska",
+        },
+      },
+      email: "adryn.khmrw@example.com",
+      login: {
+        uuid: "f085b1ec-ff0b-4b73-84b4-25fb10c16545",
+        username: "silverpanda623",
+        password: "ramses",
+        salt: "kK0MmQGF",
+        md5: "9b17970bb34cf08954e1a447b59abe16",
+        sha1: "e4556039aba60e00b808164f915e02192992412c",
+        sha256:
+          "4d1f4b66e8b7ddc93c060db0bc05440683640fa90e87fac029d92f1eb6a0e1b3",
+      },
+      dob: {
+        date: "1990-09-29T17:16:30.536Z",
+        age: 32,
+      },
+      registered: {
+        date: "2013-03-13T10:02:43.655Z",
+        age: 9,
+      },
+      phone: "062-61234556",
+      cell: "0944-262-4265",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/76.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/76.jpg",
+      },
+      nat: "IR",
+    },
+    {
+      gender: "male",
+      name: {
+        title: "Monsieur",
+        first: "Finn",
+        last: "Da Silva",
+      },
+      location: {
+        street: {
+          number: 547,
+          name: "Rue du Village",
+        },
+        city: "Pregny-Chambésy",
+        state: "Genève",
+        country: "Switzerland",
+        postcode: 8463,
+        coordinates: {
+          latitude: "-54.2545",
+          longitude: "-118.9271",
+        },
+        timezone: {
+          offset: "+11:00",
+          description: "Magadan, Solomon Islands, New Caledonia",
+        },
+      },
+      email: "finn.dasilva@example.com",
+      login: {
+        uuid: "9f93a88b-cb9d-4ac9-b923-668896639374",
+        username: "blackgoose112",
+        password: "goldie",
+        salt: "90CP8BVr",
+        md5: "8557fa562301df26dfe6365bdf6deb37",
+        sha1: "d779e190fafd788f1bdbf88713ab8cc18b99f596",
+        sha256:
+          "c9cbe2d2abf24d2e175f11b3d8c0685bcfffcd89043d90c5433c84b044c0aea7",
+      },
+      dob: {
+        date: "1965-07-19T11:06:52.230Z",
+        age: 57,
+      },
+      registered: {
+        date: "2013-02-21T20:49:20.057Z",
+        age: 9,
+      },
+      phone: "076 907 94 99",
+      cell: "077 724 47 87",
+      id: {
+        name: "AVS",
+        value: "756.2583.1641.20",
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/91.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/91.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+      },
+      nat: "CH",
+    },
+    {
+      gender: "male",
+      name: {
+        title: "Mr",
+        first: "Noah",
+        last: "Wood",
+      },
+      location: {
+        street: {
+          number: 8578,
+          name: "Lincoln Road",
+        },
+        city: "Taupo",
+        state: "Gisborne",
+        country: "New Zealand",
+        postcode: 42434,
+        coordinates: {
+          latitude: "32.4898",
+          longitude: "13.3336",
+        },
+        timezone: {
+          offset: "-9:00",
+          description: "Alaska",
+        },
+      },
+      email: "noah.wood@example.com",
+      login: {
+        uuid: "3142f0da-46ff-47af-bc46-25069ed5e0bf",
+        username: "goldenbear791",
+        password: "jonathan",
+        salt: "v0I8MemL",
+        md5: "ec397101d5a4d874e036c7d30d82543d",
+        sha1: "98018abf91f0e504c925cfa80b230f46d134a31e",
+        sha256:
+          "0472ff75ec029ca81132d77e2e83f67d4d9a8c9a538e4534606b29ded90ca79d",
+      },
+      dob: {
+        date: "1992-02-24T01:33:06.978Z",
+        age: 30,
+      },
+      registered: {
+        date: "2004-10-22T15:18:03.537Z",
+        age: 18,
+      },
+      phone: "(193)-511-9786",
+      cell: "(628)-099-6585",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/11.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/11.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/11.jpg",
+      },
+      nat: "NZ",
+    },
+    {
+      gender: "female",
+      name: {
+        title: "Ms",
+        first: "Isabelle",
+        last: "Johnston",
+      },
+      location: {
+        street: {
+          number: 9812,
+          name: "O'Connell Avenue",
+        },
+        city: "Lusk",
+        state: "Kildare",
+        country: "Ireland",
+        postcode: 90832,
+        coordinates: {
+          latitude: "-15.5166",
+          longitude: "159.9887",
+        },
+        timezone: {
+          offset: "+4:30",
+          description: "Kabul",
+        },
+      },
+      email: "isabelle.johnston@example.com",
+      login: {
+        uuid: "e8b367ce-b48d-452a-a241-d4ce2d7b355f",
+        username: "greenduck820",
+        password: "kristian",
+        salt: "rsGOLlFU",
+        md5: "ec1710477602a9c4407d57a7b01a76e4",
+        sha1: "470cc4e36af3398ed8c75620b0e7f131883c25ba",
+        sha256:
+          "2a29537a9ffcbfac357760b6874e5b49ed5571c64bfa0732d51462e71a937872",
+      },
+      dob: {
+        date: "1977-03-19T02:22:45.881Z",
+        age: 45,
+      },
+      registered: {
+        date: "2016-03-09T13:03:29.328Z",
+        age: 6,
+      },
+      phone: "031-623-0110",
+      cell: "081-267-3492",
+      id: {
+        name: "PPS",
+        value: "3413512T",
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/74.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/74.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/74.jpg",
+      },
+      nat: "IE",
+    },
+    {
+      gender: "female",
+      name: {
+        title: "Mrs",
+        first: "Alexis",
+        last: "Hawkins",
+      },
+      location: {
+        street: {
+          number: 1379,
+          name: "Camden Ave",
+        },
+        city: "Waco",
+        state: "Georgia",
+        country: "United States",
+        postcode: 39636,
+        coordinates: {
+          latitude: "9.9054",
+          longitude: "163.2596",
+        },
+        timezone: {
+          offset: "-12:00",
+          description: "Eniwetok, Kwajalein",
+        },
+      },
+      email: "alexis.hawkins@example.com",
+      login: {
+        uuid: "9139750c-e212-4f48-a1fc-e9a4d0efb8e9",
+        username: "heavysnake810",
+        password: "rusty2",
+        salt: "aMVXtR16",
+        md5: "ef0f41143d2b19b998ab3211fdcd94d4",
+        sha1: "015c900677d7168015b7ace551c94f9856e694cd",
+        sha256:
+          "b84f2351aa209c3d19ed58afe838a1f4acf3e10ceea40ec5fbf8cbf3e664692c",
+      },
+      dob: {
+        date: "1992-04-05T13:30:59.181Z",
+        age: 30,
+      },
+      registered: {
+        date: "2014-08-13T03:06:13.154Z",
+        age: 8,
+      },
+      phone: "(787)-967-8094",
+      cell: "(206)-214-6706",
+      id: {
+        name: "SSN",
+        value: "057-88-4639",
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/32.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/32.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/32.jpg",
+      },
+      nat: "US",
+    },
+    {
+      gender: "female",
+      name: {
+        title: "Ms",
+        first: "Kate",
+        last: "Morris",
+      },
+      location: {
+        street: {
+          number: 325,
+          name: "Panama Road",
+        },
+        city: "Napier",
+        state: "Hawke'S Bay",
+        country: "New Zealand",
+        postcode: 49979,
+        coordinates: {
+          latitude: "22.4462",
+          longitude: "136.1263",
+        },
+        timezone: {
+          offset: "-11:00",
+          description: "Midway Island, Samoa",
+        },
+      },
+      email: "kate.morris@example.com",
+      login: {
+        uuid: "67daf896-e448-4a93-8c86-3c9cc5a35032",
+        username: "beautifuldog797",
+        password: "bengals",
+        salt: "ihAR6uBo",
+        md5: "b2d3e7dbededad0ff268b26915f999b2",
+        sha1: "034f80f288dff3e6dbfd6f4119fe1ec6d77612c4",
+        sha256:
+          "7ee6008714f540a0c5b58f56de0074dbc0de71d9d5aeb133473c0c23161b67f9",
+      },
+      dob: {
+        date: "1958-08-17T12:41:27.000Z",
+        age: 64,
+      },
+      registered: {
+        date: "2005-05-17T02:59:38.629Z",
+        age: 17,
+      },
+      phone: "(671)-635-7541",
+      cell: "(717)-319-0609",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/2.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/2.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/2.jpg",
+      },
+      nat: "NZ",
+    },
+    {
+      gender: "female",
+      name: {
+        title: "Mrs",
+        first: "Ömür",
+        last: "Nebioğlu",
+      },
+      location: {
+        street: {
+          number: 7769,
+          name: "Talak Göktepe Cd",
+        },
+        city: "Siirt",
+        state: "Isparta",
+        country: "Turkey",
+        postcode: 85641,
+        coordinates: {
+          latitude: "-88.8302",
+          longitude: "-97.7073",
+        },
+        timezone: {
+          offset: "-2:00",
+          description: "Mid-Atlantic",
+        },
+      },
+      email: "omur.nebioglu@example.com",
+      login: {
+        uuid: "9c67f366-1582-43fe-8670-21f2509cc5bd",
+        username: "crazypanda951",
+        password: "butterfl",
+        salt: "0Ozoc2Ad",
+        md5: "740b59807feca0d4e8c4465e81eed9ba",
+        sha1: "0e1c06fdd3462987dfbd30934731210107ce19f4",
+        sha256:
+          "f1231be963730702bdbb6fa4b8489eb7c743e98005ff42032b7bde2eec38a6d1",
+      },
+      dob: {
+        date: "1994-08-01T06:21:33.461Z",
+        age: 28,
+      },
+      registered: {
+        date: "2005-02-08T11:56:34.191Z",
+        age: 17,
+      },
+      phone: "(847)-559-7245",
+      cell: "(165)-587-2622",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/22.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/22.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/22.jpg",
+      },
+      nat: "TR",
+    },
+    {
+      gender: "male",
+      name: {
+        title: "Mr",
+        first: "پارسا",
+        last: "قاسمی",
+      },
+      location: {
+        street: {
+          number: 5853,
+          name: "آذربایجان",
+        },
+        city: "نیشابور",
+        state: "خراسان شمالی",
+        country: "Iran",
+        postcode: 95426,
+        coordinates: {
+          latitude: "77.4946",
+          longitude: "97.6839",
+        },
+        timezone: {
+          offset: "+11:00",
+          description: "Magadan, Solomon Islands, New Caledonia",
+        },
+      },
+      email: "prs.qsmy@example.com",
+      login: {
+        uuid: "7cceb5cc-cf9c-4d1e-b30a-326ab0a09bcd",
+        username: "tinyduck153",
+        password: "jared",
+        salt: "8qd2okWS",
+        md5: "db8b6ba47b8a6fa2702293461d18a9c8",
+        sha1: "0338451c86fd43a9bf33e6e5d9818e67d10ac2e4",
+        sha256:
+          "b86ee92b9cf62fab0888f006a22938769155e8b8c7a08ca2dbf743d2faaac0f4",
+      },
+      dob: {
+        date: "1950-01-22T19:41:39.733Z",
+        age: 72,
+      },
+      registered: {
+        date: "2018-04-19T03:16:09.838Z",
+        age: 4,
+      },
+      phone: "028-43138741",
+      cell: "0948-774-0763",
+      id: {
+        name: "",
+        value: null,
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/35.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/35.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/35.jpg",
+      },
+      nat: "IR",
+    },
+  ],
+  info: {
+    seed: "1881cd75ad79a481",
+    results: 10,
+    page: 1,
+    version: "1.3",
+  },
+};
