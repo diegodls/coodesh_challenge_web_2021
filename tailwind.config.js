06349e6199904fb039f8c05dbff3340f6466b7a5
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-08-05 15:19:54 -0300

    css: changing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    content: ["./src/**/*.tsx"],
-    theme: {
-        extend: {
-            colors: {
-                pharma: {
-                    primary: "#f3f3f3",
-                    secondary: "#FFFFFF",
-                    txt_primary: "#252A37",
-                    txt_secondary: "###78819B",
-                    enabled: "##0029FF",
-                    disabled: "#ECE6FE",
-                    hover: "#FBF7F0",
-                    border_enable: "#6a7186",
-                    border_disabled: "#ECE6FE",
-                    border_focus: "#0029FF",
-                    error_primary: '#EB5353'
-                },
-            },
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        pharma: {
+          primary: "#f3f3f3",
+          secondary: "#FFFFFF",
+          txt_primary: "#252A37",
+          txt_secondary: "#9C9D9E",
+          enabled: "#0283C5",
+          disabled: "#c2c2c2",
+          hover: "#189de0",
+          border_enabled: "#6a7186",
+          border_disabled: "#c2c2c2",
+          border_focus: "#0283C5",
+          error_primary: "#EB5353",
         },
+      },
     },
-    plugins: [require("@tailwindcss/forms"), require('tailwind-scrollbar'), ],
+  },
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
